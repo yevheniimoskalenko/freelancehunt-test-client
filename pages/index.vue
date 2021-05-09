96322d559d99d1c2b234f5972756eb0b0f23c8a7
testjokerqwerty@gmail.com <testjokerqwerty@gmail.com>
2021-05-09 23:33:57 +0300

    add price

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <div class=""></div>
+  <div class="container">
+    <el-table :data="posts" style="width: 100%">
+      <el-table-column prop="title" label="title"> </el-table-column>
+      <el-table-column label="categories">
+        <template slot-scope="scope">{{
+          scope.row.categories.join(', ')
+        }}</template>
+      </el-table-column>
+      <el-table-column label="title">
+        <template slot-scope="scope">{{
+          scope.row.title.match(/\d+[(₴|₽)]/g)
+        }}</template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
-
 <script>
-export default {}
+export default {
+  filters: {
+    price(str) {
+      return str
+    },
+  },
+  data() {
+    return {}
+  },
+  async fetch({ store }) {
+    await store.dispatch('getList')
+  },
+  computed: {
+    posts() {
+      return this.$store.getters.getPosts
+    },
+  },
+}
 </script>
